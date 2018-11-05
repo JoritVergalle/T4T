108b6d31d83168ee1c0d04e1bc4e03bb16bc2515
JoritPc <Jorit.vergalle@hotmail.com>
2018-11-05 17:10:39 +0100

    can add talks & reads txtfile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TalksComponent } from './talks/talks.component';
 import { TalksAddComponent } from './talks-add/talks-add.component';
 import { TrackComponent } from './track/track.component';
-import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
--- src/app/talks/talks.component.ts
+++ src/app/talks/talks.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Talk } from '../models/talk.model';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-declare var $: any;
 
 @Component({
   selector: 'app-talks',
@@ -9,15 +8,16 @@ declare var $: any;
   styleUrls: ['./talks.component.css']
 })
 export class TalksComponent implements OnInit {
-  private _talks: Talk[];
+  private _talks = new Array<Talk>();
   talk: FormGroup;
+  file: any;
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     this.talk = this.fb.group({
-      title: [null, [Validators.required]],
-      length: [null, [Validators.required, Validators.minLength(2)]]
+      length: [null, [Validators.required]],
+      title: [null, [Validators.required, Validators.minLength(2)]]
     });
   }
 
@@ -27,11 +27,51 @@ export class TalksComponent implements OnInit {
   }
 
   openAddTalk() {
-    this.talk.reset();
-    $('.ui.modal.addTalk').modal('show');
+    // this.talk.reset();
+    // $('.ui.modal.addTalk').modal('show');
   }
 
   addTalk() {
-    console.log('add');
+  if (this.talk.valid) {
+      const talk = new Talk(this.talk.value.title, this.talk.value.length);
+      this._talks.push(talk);
+      this.talk.reset();
+    }
+  }
+  // readTxt() {
+  //   console.log('Hi');
+  // }
+
+  fileChanged(e) {
+    this.file = e.target.files[0];
+  }
+
+  readTxt() {
+    // TODO RD: https://stackoverflow.com/questions/4416425/how-to-split-string-with-some-separator-but-without-removing-that-separator-in-j
+    const fileReader = new FileReader();
+    // fileReader.onload = (e) => {
+    //   console.log(fileReader.result);
+    // };
+    fileReader.onload = (event) => {
+      const file = event.target.result;
+      const allLines = file.split(/\r\n|\n/);
+      // Reading line by line
+      allLines.forEach((line) => {
+        // const talk = new Talk(line, 50);
+        // this._talks.push(talk);
+        // const titleNumber = line.split(/(?=[0-9]')/);
+        // const titleNumber = line.split(/[0-9](.+)/, 2);
+        const talkArray = line.split(/(?=[0-9])(.+)|lightning/, 2);
+        console.log(talkArray);
+        let talk;
+        if (talkArray[1] === undefined) {
+          talk = new Talk(talkArray[0], 5);
+        } else {
+          talk = new Talk(talkArray[0], parseInt(talkArray[1], 10));
+        }
+        this._talks.push(talk);
+      });
+    };
+    fileReader.readAsText(this.file);
   }
 }
