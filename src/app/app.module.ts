8595df5c8f946c0a20bf5824490ba7115ecffabc
JoritPc <Jorit.vergalle@hotmail.com>
2018-11-08 15:47:23 +0100

    Fixes the two issues that caused the first compile to fail.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,11 @@ import {ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TalksComponent } from './talks/talks.component';
-import { TalksAddComponent } from './talks-add/talks-add.component';
-import { TrackComponent } from './track/track.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TalksComponent,
-    TalksAddComponent,
-    TrackComponent
+    TalksComponent
   ],
   imports: [
     BrowserModule,
